ef9bf7c982882f6d45f7e1634a7ae2b942550d13
surya-prakash-p <suryapraks588@gmail.com>
2026-02-24 18:15:54 +0530

    fix 400 bad request on render

--- inventory_project/inventory_project/settings.py
+++ inventory_project/inventory_project/settings.py
@@ -25,11 +25,14 @@ SECRET_KEY = 'django-insecure-$e@*6y&$mo6s9$+r8@e_jbgi%z_!ygs@lxn&yu$=gwy#%+%b=z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    'smart-stock-inventory-system-4.onrender.com',
-    'localhost',
-    '127.0.0.1',
+ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://smart-stock-inventory-system-4.onrender.com",
+    "https://*.onrender.com",
 ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
