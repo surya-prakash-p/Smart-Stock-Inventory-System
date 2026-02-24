be10977f131ba741e84c0ecae58539ea1430abb3
surya-prakash-p <suryapraks588@gmail.com>
2026-02-24 17:53:40 +0530

    fix allowed hosts

--- inventory_project/inventory_project/settings.py
+++ inventory_project/inventory_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$e@*6y&$mo6s9$+r8@e_jbgi%z_!ygs@lxn&yu$=gwy#%+%b=z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
