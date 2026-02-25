a3d1882df53fcfcd6d1f9cafc03e89a99aeca104
surya-prakash-p <suryapraks588@gmail.com>
2026-02-25 15:46:14 +0530

    fix: use sqlite on render

--- inventory_project/inventory_project/settings.py
+++ inventory_project/inventory_project/settings.py
@@ -81,19 +81,28 @@ WSGI_APPLICATION = 'inventory_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import pymysql
-pymysql.install_as_MySQLdb()
+import os
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'smart_inventory_db',   # your DB name
-        'USER': 'root',                 # your mysql user
-        'PASSWORD': 'rootroot',    # your mysql password
-        'HOST': 'localhost',
-        'PORT': '3306',
+if os.getenv("RENDER"):
+    # ✅ Render → use SQLite (stable)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    # ✅ Local → use MySQL
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'smart_inventory_db',
+            'USER': 'root',
+            'PASSWORD': 'YOUR_PASSWORD',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
+    }
 
 
 # Password validation
